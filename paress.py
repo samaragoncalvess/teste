79210a64ad533967201e9073c67e0d1b6d326953
samaragoncalvess <samarasilva222220@gmail.com>
2025-04-29 20:00:15 -0300

    remov

--- paress.py
+++
@@ -1,9 +0,0 @@
-soma = 0
-for n in range(6):
-    try:
-        n = int(input('Digite os números: '))
-        if n % 2 == 0:
-            soma += n
-    except ValueError:
-        print("Por favor, insira um número válido.")
-print(f'A soma de todos os valores é: {soma}')
